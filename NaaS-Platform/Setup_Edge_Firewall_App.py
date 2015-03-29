8b002d7ec0f12a3e43a698e979f6054d3a764054
Mani Prashanth Varma Manthena <prashanthvarma.manthena@gmail.com>
2015-03-29 21:58:20 +0200

    Added Licence statement to the source code files

--- NaaS-Platform/Setup_Edge_Firewall_App.py
+++ NaaS-Platform/Setup_Edge_Firewall_App.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# #Copyright (C) 2015, Delft University of Technology, Faculty of Electrical Engineering, Mathematics and Computer Science, Network Architectures and Services and TNO, ICT - Service Enabling and Management, Mani Prashanth Varma Manthena, Niels van Adrichem, Casper van den Broek and F. A. Kuipers
+#
+# This file is part of NaaSPlatform.
+#
+# NDNFlow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# NDNFlow is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NaaSPlatform. If not, see <http://www.gnu.org/licenses/>.
+
 
 
 # Initializing and running the Network-as-a-Service (NaaS) platform's edge firewall application
