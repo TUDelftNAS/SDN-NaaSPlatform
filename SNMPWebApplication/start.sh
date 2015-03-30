d4eb0b1e2253d007fcebbf2e17ec0cc93e5c28c2
Mani Prashanth Varma Manthena <prashanthvarma.manthena@gmail.com>
2015-03-30 19:13:24 +0200

    Added Copyright statement to source code files

--- SNMPWebApplication/start.sh
+++ SNMPWebApplication/start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# # #Copyright (C) 2015, Delft University of Technology, Faculty of Electrical Engineering, Mathematics and Computer Science, Network Architectures and Services and TNO, ICT - Service Enabling and Management, Mani Prashanth Varma Manthena, Niels van Adrichem, Casper van den Broek and F. A. Kuipers
+# #Copyright (C) 2015, Delft University of Technology, Faculty of Electrical Engineering, Mathematics and Computer Science, Network Architectures and Services and TNO, ICT - Service Enabling and Management, Mani Prashanth Varma Manthena, Niels van Adrichem, Casper van den Broek and F. A. Kuipers
 #
 # This file is part of NaaSPlatform.
 #
-# NDNFlow is free software: you can redistribute it and/or modify
+# NaaSPlatform is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# NDNFlow is distributed in the hope that it will be useful,
+# NaaSPlatform is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
